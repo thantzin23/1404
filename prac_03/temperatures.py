5efcd3d3d9c66a2e6b6e3a4db4f238127f37dc6f
thantzin23 <thantzin23523@gmail.com>
2022-04-01 13:45:53 +0800

    add refector temperature

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -1,32 +1,26 @@
-def main():
-    menu = ("C - convert celsius to fahrenheit , F - convert fahrenheit to celsius , Q - Quit")
-    print(menu)
-    choice = calculate_tem()
-    choice = input("Choice").upper()
-
-def calculate_tem(choice):
-    while choice != "Q":
-        if choice == "C":
+Menu = ("C - convert celsius to fahrenheit , F - convert fahrenheit to celsius , Q - Quit")
+print(Menu)
+choice = input("Choice").upper()
+while choice != "Q":
+    if choice == "C":
             celsius = float(input("Enter temperature of celsius:"))
             fahrenheit = celsius * 9 / 5 + 32
             print (f"Result is :{fahrenheit:.2f}")
 
-        elif choice == "F":
+    elif choice == "F":
             fahrenheit = float(input("Enter temperature of Fahrenheit:"))
             celsius = fahrenheit *(5 + 32) / 9
             print (f"Result is :{celsius:.2f}")
 
-        else:
+    else:
             print("Invaild options!")
-            menu = ("C - convert celsius to fahrenheit , F - convert fahrenheit to celsius , Q - Quit")
-            print(menu)
-            choice = input("Choice").upper()
+    print(Menu)
+    choice = input("Choice").upper()
 
-    print("Thank You!")
-
+print("Thank You!")
 
 
 
 
 
-main()
+
