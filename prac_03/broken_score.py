5cb8c0bc025371c17c9b680f630acc376d23d010
thantzin23 <thantzin23523@gmail.com>
2022-04-01 13:53:55 +0800

    add broken score

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,5 +1,21 @@
 def main():
+    score = float(input("Enter your scores:"))
 
-    pass
+    while score < 0 or score > 100 :
+        print ("Invalid scores!")
+        score = float(input("Enter your scores:"))
+
+    print("Your entered:",score)
+
+    if score >= 50 :
+        print("You pass!")
+    elif score >=85 :
+        print("Excellent!")
+    else:
+        print("Ah..oh")
+
+
+
+
 
 main()
