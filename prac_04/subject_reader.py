e9c427484602c471adf6d4c40553a20ec563dcc1
thantzin23 <thantzin23523@gmail.com>
2022-04-08 10:44:48 +0800

    modify subject_reader.py

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -6,24 +6,35 @@ Data file -> lists program
 FILENAME = "subject_data.txt"
 
 
-def main():
-    data = get_data()
-    print(data)
+def main():# in main step put all def funtion befor def 
+    subject_details = get_data()
+    print_subject (subject_details)
+    #if there is any menu..
+    #get user input
+    #while user_input !="Q"
+    #  if ..
+    #     do_funtion(1)
+    #  elif...
+    #     do_funtion(2)
+    # .....
+    #  get user input
 
+def print_subject(subject_details):
+    for subject_detail in subject_details:
+        print(f"{subject_detail[0]}is taught by {subject_detail[1]:12} and has {subject_detail[-1]:3} students")
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject_detail =[]
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject_detail.append(parts)
+
     input_file.close()
+    return subject_detail # while return , delet all printing steps
 
 
 main()
