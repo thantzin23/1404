f34d6ec79c71fd384cd72fd18d81bd7572ec2cb0
thantzin23 <thantzin23523@gmail.com>
2022-04-08 09:17:47 +0800

    add lists_warmup.py

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,17 @@
+#numbers[0]
+#numbers[-1]
+#numbers[3]
+#numbers[:-1]
+#numbers[3:4]
+#5 in numbers
+#7 in numbers
+#"3" in numbers
+#numbers + [6, 5, 3]
+
+numbers = ["Ten", 1, 4, 1, 5, 9, 1]
+
+print(numbers[2:])
+print(numbers[5:6])
+
+
+
