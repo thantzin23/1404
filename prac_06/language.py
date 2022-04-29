ec2229ddfaf171933660a8398d6af3cd172e1116
thantzin23 <thantzin23523@gmail.com>
2022-04-29 12:01:04 +0800

    commit programing_language.py and language.py

---
+++ prac_06/language.py
@@ -0,0 +1,16 @@
+from prac_06.programing_language import ProgrammingLanguage
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [ruby,python,visual_basic]
+    for language in languages :
+        if language.is_dynamic():
+            print(language.name)
+
+
+
+main()
+
---
+++ prac_06/programing_language.py
@@ -0,0 +1,32 @@
+
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
+
+def tests_language():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [ruby, python, visual_basic]
+    print(python)
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+
+tests_language()
